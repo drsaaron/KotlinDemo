900a28008cd0be394c2bf28c3cc2ce213311b6dd
AARON <scottaaron@northwesternmutual.com>
2024-04-11 10:57:55 -0500

    version update

---
+++ src/main/kotlin/com/blazartech/kotlindemo/HelloWorldCommandLineRunner.kt
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Kotlin.kt to edit this template
+ */
+
+package com.blazartech.kotlindemo
+
+import org.springframework.boot.CommandLineRunner
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.beans.factory.annotation.Value
+
+@Component
+class HelloWorldCommandLineRunner : CommandLineRunner {
+
+    private val log = LoggerFactory.getLogger(HelloWorldCommandLineRunner::class.java)
+
+    @Value("\${demo.msg}") 
+    private val message : String = ""
+
+    override fun run(vararg args: String?) {
+        args.forEach { log.info("Application args: $it") }
+        log.info("Hello world!")
+        log.info("message property = {}", message)
+    }
+}
+
---
+++ src/main/kotlin/com/blazartech/kotlindemo/KotlinDemo.kt
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Kotlin.kt to edit this template
+ */
+
+package com.blazartech.kotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDemo
+
+fun main(args: Array<String>) {
+    runApplication<KotlinDemo>(*args)
+}
